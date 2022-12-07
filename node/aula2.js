2866341cac87fbc4b37733f021a1dd3ad758654b
Raphael Lameri <104907538+raphalameri@users.noreply.github.com>
2022-12-07 09:09:10 -0300

    Aula 2

---
+++ node/aula2.js
@@ -0,0 +1,12 @@
+var http = require ('http');
+var url = require('url');
+
+//mudando o servidor 
+http.createServer (function (req, res) {
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = "ano: " + q.year + " mes: " +q.month; 
+    res.end(txt);
+
+}).listen(80);
+
